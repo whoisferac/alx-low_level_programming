32d9742c90e6056aecdf0378e578a5052fb05615
emmanuel <attueyiemmanuel2322@gmail.com>
2022-09-09 05:43:46 +0100

    more

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpat, 2015-10-19\n" 61);
-	return (0);
+	fwrite(2, "and that piece of art is useful\" - Dora Korpat, 2015-10-19\n" 61);
+	return (1);
 }
