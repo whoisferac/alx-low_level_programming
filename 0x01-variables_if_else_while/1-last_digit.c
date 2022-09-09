32e611e63554763f577904cf305139eb86443310
emmanuel <attueyiemmanuel2322@gmail.com>
2022-09-10 00:27:30 +0100

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,8 +26,8 @@ printf("Last digit of %d is %d and is 0\n", n,feracSpecialNumber);
 }
 if (feracSpecialNumber < 6 && feracSpecialNumber != 0)
 {
-printf("Last digit of %d is %d and is less than 
-6 and not 0\n", n, feracSpecialNumber);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n,
+feracSpecialNumber);
 }
 return (0);
 }
