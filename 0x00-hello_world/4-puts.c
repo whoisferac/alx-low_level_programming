aedab9ac5eb7a4074b1f6bb007e785655d405754
emmanuel <attueyiemmanuel2322@gmail.com>
2022-09-09 04:40:12 +0100

    more

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
  * main : printing out string Programming is like building ... i *nto standard output
  * Return: 0 after printing the function
  */
+
 int main (void)
 {
-	puts("\"
-	Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
