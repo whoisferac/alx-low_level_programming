9e935cad319d9f34cd9bfdc2468048d074932316
emmanuel <attueyiemmanuel2322@gmail.com>
2022-09-09 04:27:51 +0100

    more c programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - printing string with printf
+ * return 0 if corect
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+ * main - prints the size of different variables
+ * return - 0 ( if correct)
+ * camel casing used 
+ */
+int main (void)
+{
+	char charType;
+	int intType;
+	long int longIntType;
+	long long int longLongIntType;
+	float floatType;
+
+	printf("size of a char: %lu byte(s)\n", sizeof(charType));
+	printf("size of an int: %lu byte(s)\n", sizeof(intType));
+	printf("size of a long int: %lu byte(s)\n", sizeof(longIntType));
+	printf("size of a long Long Int: %lu byte(s)\n", sizeof(longLongIntType));
+        printf("size of a float: %lu byte(s)\n", sizeof(floatType));
+return (0);
+}
