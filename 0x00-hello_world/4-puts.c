545f1961ef92b444340b91ae108940dffd4f562f
emmanuel <attueyiemmanuel2322@gmail.com>
2022-09-09 03:47:15 +0100

    error fixed in 4-puts.c"

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /*
- * main - entry point
- *
+ * main - printing out string Programming is like building ... i *nto standard output
  * Return: 0 after printing the function
  */
 int main (void)
-{puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+{
+	puts("\"
+	Programming is like building a multilingual puzzle");
+	return (0);
 }
