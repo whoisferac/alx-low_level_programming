4d9e3baa12b09991242ea6a7f375bb7e70434a7e
emmanuel <attueyiemmanuel2322@gmail.com>
2022-09-09 05:39:44 +0100

    more

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
 
 /**
- *
+ * function: main printing without printf and puts
+ * description: printing without printf and puts
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpat, 2015-10-19\n" 61);
+	return (0);
+}
