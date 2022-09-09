3a4744543e6cf7e7d0de5622c8f3475e31c7d2e3
emmanuel <attueyiemmanuel2322@gmail.com>
2022-09-10 00:21:42 +0100

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/* main - entry point
+/**
+ * main - entry point
  *
  * Description: a program that performs so unique functions
  *
@@ -16,15 +17,17 @@ n = rand() - RAND_MAX / 2;
 feracSpecialNumber = n % 10;
 if (feracSpecialNumber > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n , feracSpecialNumber);
+printf("Last digit of %d is %d and is greater than 5\n"
+,n,feracSpecialNumber);
 }
 if (feracSpecialNumber == 0)
 {
-printf("Last digit of %d is %d and is 0\n", n , feracSpecialNumber);
+printf("Last digit of %d is %d and is 0\n",n,feracSpecialNumber);
 }
 if (feracSpecialNumber < 6 && feracSpecialNumber != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n , feracSpecialNumber);
+printf("Last digit of %d is %d and is less than 
+6 and not 0\n",n,feracSpecialNumber);
 }
 return (0);
 }
