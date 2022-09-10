b6572504993efc807192cd284a2c6bdcadd794f9
emmanuel <attueyiemmanuel2322@gmail.com>
2022-09-10 02:13:24 +0100

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Description: a program in c to print numbers 0 to 9
+ * Return: 0 else 1
+*/
+int main(void)
+{
+int num = 0;
+int ten = 10;
+while (num < ten)
+{
+printf("%d\n", num);
+num++;
+}
+return (0);
+}
