57cddf55331d6b77d299acf543536ac7c5573bfb
emmanuel <attueyiemmanuel2322@gmail.com>
2022-09-14 04:50:53 +0100

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *main - void
+ *Description: printd alphabets
+ *Return: Always 0.
+*/
+void print_alphabet(void)
+{
+int z;
+for(z = 'a'; z <= 'z'; z++);
+{
+_putchar('z');
+}
+}	
