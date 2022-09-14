7b82a3d7492bcc9a11e3227e6483f9774326b66d
emmanuel <attueyiemmanuel2322@gmail.com>
2022-09-14 21:12:31 +0100

    task11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints 0 - 98
+ *@n: integer
+*/
+
+void print_to_98(int n)
+{
+if (n < 98)
+{
+for (n < 98; n++)
+{
+printf("%d, ",n);
+}
+else
+{
+for (n > 98; n--)
+{
+printf("%d, ",n);
+}
+}
+printf("%d\n",n);
+}
