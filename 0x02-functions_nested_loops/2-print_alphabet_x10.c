6816ec5c663b7b6b4b263d8dcf0416fd47ea4175
emmanuel <attueyiemmanuel2322@gmail.com>
2022-09-14 05:18:11 +0100

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10(void) - prints the alphabet 10 times
+ * Description: prints alphabet
+ * Return: empty
+*/
+void print_alphabet_x10(void)
+{
+int q;
+int w;
+
+for (q = 1; q <= 10; q++)
+{
+for (w = 'a'; w <= 'z'; w++)
+_putchar(w);
+_putchar('\n');
+}
+}	
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 #define main_h_
 int _putchar(int c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
