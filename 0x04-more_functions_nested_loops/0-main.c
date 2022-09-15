43980f387923ab07564c332ee0bb5f31bc8db79a
emmanuel <attueyiemmanuel2322@gmail.com>
2022-09-15 14:16:46 +0100

    task0

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -8,11 +8,10 @@
  */
 int main(void)
 {
-    char c;
-
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+char c;
+c = 'A';
+printf("%c: %d\n", c, _isupper(c));
+c = 'a';
+printf("%c: %d\n", c, _isupper(c));
+return (0);
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,2 @@
-#ifndef main_h_
 #define main_h_
 int _isupper(int c);
