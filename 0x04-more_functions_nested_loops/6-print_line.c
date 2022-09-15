84887e1e810c6b684c410a4462c7ecbf86a3e3a1
emmanuel <attueyiemmanuel2322@gmail.com>
2022-09-15 20:35:21 +0100

    task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - prints lines in the terminal
+ * @n: variable
+*/
+void print_line(int n)
+{
+int z;
+if (z > 1)
+{
+for (z = 1; z <= n; z++)
+{
+-putchar('_');
+}
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
+}
