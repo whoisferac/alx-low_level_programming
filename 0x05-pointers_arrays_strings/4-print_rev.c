c29fd970e075e9dd070df9fa0672e6097f49a331
emmanuel <attueyiemmanuel2322@gmail.com>
2022-09-20 08:00:07 +0300

    rush

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints in reverse
+ * @s: pointer
+*/
+void print_rev(char *s)
+{
+int z, v = 0;
+for (; s[v] != '\0'; v++)
+{
+}
+for (z = c - 1; z >= 0; z--)
+{
+_putchar(s[z]);
+}
+_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+void rev_string(char *c)
+{
+int a, b, c, d;
+for (b = 0; s[b] != '\0'; b++)
+{
+}
+c = b - 1;
+for (a = 0; a < c; c--)
+{
+d = s[b];
+s[b] = s[a];
+s[a] = d;
+c++;
+}
+}
